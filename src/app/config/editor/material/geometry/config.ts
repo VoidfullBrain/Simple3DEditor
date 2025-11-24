695969f5df1217e2dee9bfbcd6bc356967d9ce5f
Ron Schalla <152904188+VoidfullBrain@users.noreply.github.com>
2025-11-24 21:39:41 +0100

    Updated config.ts

--- src/app/config/editor/material/geometry/config.ts
+++ src/app/config/editor/material/geometry/config.ts
@@ -1,7 +1,5 @@
 export const config = {
   color: 0x55a1d4,
-  transparent: true,
-  opacity: 0.3,  // Semi-transparent so faces are visible but edges stand out
   polygonOffset: true,
   polygonOffsetFactor: 1,
   polygonOffsetUnits: 1
